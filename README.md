1240bc7d55ff6083cbdf50c638a3f826b012a3ee
Bailee Manzer <baileemanzer3@gmail.com>
2019-10-19 20:38:30 -0500

    Psychic Game

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Psychic-Game
+
+## About:
+Guess the random letter and win a point. You only get 10 chances to guess or you'll lose a point. 
+
+## Demo:
+Here is the link to my Psychic-Game application--> 
+
+## Build Tools: 
+Javascript
+HTML
+CSS
--- assets/game.js
+++ assets/game.js
@@ -1,10 +1,40 @@
-<!DOCTYPE html>
-<html lang="en-us">
-  <head>
-    <meta charset="UTF-8">
-    <title>Psychic Game</title>
-  </head>
-  <body>
+var ComputerChoices=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-</body>
-</html>
+        var WinCount=0;
+        var LossCount=0;
+        var GuessLeft=10;
+        var GuessedLetters= [];
+        var ranLetter="";
+        var usedArray=[];
+
+
+        function CompGuess() {
+            ranLetter= ComputerChoices[Math.floor(Math.random()*ComputerChoices.length)];
+        }
+        CompGuess();
+    document.onkeyup=function (event) {
+          
+        var userGuess = event.key.toLowerCase();
+
+        if (userGuess===ranLetter) {
+            WinCount++;
+            GuessLeft=10;
+            CompGuess();
+        } else if (usedArray.indexOf(userGuess) ===-1){
+            GuessLeft--;
+            usedArray.push(userGuess);
+        }
+        else {
+            alert("You've already guessed that!");
+        }
+        if (GuessLeft==0) {
+            LossCount++;
+            GuessLeft=10;
+            CompGuess();
+        }
+
+        document.getElementById('WinCount').innerHTML=WinCount;
+        document.getElementById('LossCount').innerHTML=LossCount;
+        document.getElementById('GuessLeft').innerHTML=GuessLeft;
+        document.getElementById('userGuess').innerHTML = usedArray;
+    }
